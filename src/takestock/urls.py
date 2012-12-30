ddf8d179257dbda69d1c760d1d82d0bd9ffac8d0
Joe Strickler <joe@decafjoe.com>
2012-12-29 21:56:56 -0500

    Fixes E128 error (some indentation thing).

--- src/takestock/urls.py
+++ src/takestock/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include, patterns, url
 from django.views.generic import DetailView, ListView
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^$', 'takestock.views.index'),
     url(r'^clubs/$', 'takestock.views.club_general', name='club_general'),
     url(r'^clubs/(?P<club_id>\d+)/$', 'takestock.views.club_detail', name='club_detail'),               
