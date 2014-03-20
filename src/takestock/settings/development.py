64ee2f3a97f9b0f221fbf9c377b8faaeb4583772
Jackson Bierfeldt <jbierfeldt@gmail.com>
2014-03-20 15:36:42 -0500

    Changed structure for dev data

--- src/takestock/settings/development.py
+++ src/takestock/settings/development.py
@@ -10,19 +10,19 @@ from takestock.settings.common import *
 
 settings_path = os.path.abspath(os.path.dirname(__file__))
 root_path = os.path.dirname(os.path.dirname(os.path.dirname(settings_path)))
-virtualenv_path = os.path.join(root_path, '.virtualenvs', 'development')
+data_path = os.path.join(root_path, 'data')
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(root_path, 'takestock.db'),
+        'NAME': os.path.join(data_path, 'takestock.db'),
     }
 }
 
 DEBUG = True
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
-EMAIL_FILE_PATH = os.path.join(virtualenv_path, 'mail')
-MEDIA_ROOT = os.path.join(virtualenv_path, 'media')
+EMAIL_FILE_PATH = os.path.join(data_path, 'mail')
+MEDIA_ROOT = os.path.join(data_path, 'media')
 SECRET_KEY = 'abc123'
-STATIC_ROOT = os.path.join(virtualenv_path, 'static')
+STATIC_ROOT = os.path.join(data_path, 'static')
 TEMPLATE_DEBUG = DEBUG
